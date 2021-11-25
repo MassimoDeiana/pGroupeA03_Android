fff229d8820ac52bec9df61ab9d40d9439637f68
WDFAllan <71563267+WDFAllan@users.noreply.github.com>
2021-11-25 11:18:20 +0100

    set up android

---
+++ app/src/main/java/com/example/pgroupea03_android/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.pgroupea03_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCamera;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.btn_cameraButton_mainActivity);
+
+
+    }
+}
